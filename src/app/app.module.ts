69528dafd7c55f9bcdb429aed97a581bee848321
Edson Carlos <edsoncarlos.dev@gmail.com>
2023-01-20 17:35:32 -0300

    feat: add icons with MatIconModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+
 import { MenuComponent } from './menu/menu.component'
 
 @NgModule({
@@ -14,7 +16,8 @@ import { MenuComponent } from './menu/menu.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+		MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
