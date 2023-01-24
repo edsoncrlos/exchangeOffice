1b04e28bb5e7da03c12aeed9d9b3ecd262c342b3
Edson Carlos <edsoncarlos.dev@gmail.com>
2023-01-24 17:21:31 -0300

    feat: add coin-listing component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CoinListingComponent } from './coin-listing/coin-listing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    CoinListingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/coin-listing/coin-listing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coin-listing',
+  templateUrl: './coin-listing.component.html',
+  styleUrls: ['./coin-listing.component.css']
+})
+export class CoinListingComponent {
+
+}
