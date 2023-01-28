4a009663c924a194a002f22eaec2a447b1d8fc86
Edson Carlos <edsoncarlos.dev@gmail.com>
2023-01-28 15:58:28 -0300

    feat: add coin-listing component and matTable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -23,6 +28,12 @@ import { CoinListingComponent } from './coin-listing/coin-listing.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
     CoreModule,
     SharedModule,
   ],
--- src/app/coin-listing/coin-listing.component.ts
+++ src/app/coin-listing/coin-listing.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
+
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort} from '@angular/material/sort';
 
 import { ExchangeRateService } from '../core/exchange-rate.service';
 import { Coin } from '../core/exchange-rate-api.interfaces';
@@ -9,23 +13,50 @@ import { Coin } from '../core/exchange-rate-api.interfaces';
   templateUrl: './coin-listing.component.html',
   styleUrls: ['./coin-listing.component.css']
 })
-export class CoinListingComponent implements OnInit, OnDestroy {
-  response!: Subscription;
+export class CoinListingComponent implements OnInit, OnDestroy, AfterViewInit {
   coins: Coin[] = [];
+  response!: Subscription;
+
+  displayColumns: string[] = ['code','description'];
+  dataSource!: MatTableDataSource<Coin>;
+  filter: string = '';
+  pageSize = 5;
 
   constructor (
     private exchangeRate: ExchangeRateService
   ) { }
 
+
   ngOnInit(): void {
-    this.response = this.exchangeRate.getCoins().subscribe((response) => {
+  this.response = this.exchangeRate.getCoins()
+    .subscribe((response) => {
       if (response.success === true) {
         this.coins = Object.values(response.symbols)
+        this.dataSource = new MatTableDataSource<Coin>(this.coins);
       }
     })
   }
 
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    }, 150)
+  }
+
   ngOnDestroy() {
     this.response.unsubscribe();
+  }
+
+  handlePageEvent() {
+    if (this.pageSize && this.pageSize >  0 && this.pageSize < this.coins.length) {
+      this.paginator._changePageSize(this.pageSize);
+    }
+  }
+
+  applyfilter() {
+    this.dataSource.filter = this.filter;
   }
 }
