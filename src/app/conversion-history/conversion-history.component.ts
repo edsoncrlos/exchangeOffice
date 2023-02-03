a434f8bfbb11a65b2b7d75b4ec5fd6a8a722657e
Edson Carlos <edsoncarlos.dev@gmail.com>
2023-02-03 15:40:46 -0300

    feat: show historic with mat-table

--- src/app/conversion-history/conversion-history.component.ts
+++ src/app/conversion-history/conversion-history.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnInit} from '@angular/core';
+
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort} from '@angular/material/sort';
+
+import { SessionStorageService } from '../core/session-storage.service';
+import { Historic } from './conversion-history.interfaces';
 
 @Component({
   selector: 'app-conversion-history',
   templateUrl: './conversion-history.component.html',
   styleUrls: ['./conversion-history.component.css']
 })
-export class ConversionHistoryComponent {
+export class ConversionHistoryComponent implements OnInit, AfterViewInit {
+  historic: Historic[] = [];
+  displayColumns: string[] = ['date', 'hourAndMinutes', 'amount', 'originCoin', 'destinationCoin', 'result', 'rate'];
+  dataSource!: MatTableDataSource<Historic>;
 
+  constructor(
+    private sessionStorage: SessionStorageService<Historic>
+  ) {}
+
+  ngOnInit() {
+    this.historic = this.sessionStorage.getItem('historic');
+    this.dataSource = new MatTableDataSource<Historic>(this.historic);
+  }
+
+  @ViewChild(MatSort) sort!: MatSort;
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
 }
